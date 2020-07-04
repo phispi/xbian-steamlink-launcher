2e70b81e84688adb57c4e476f3e1aa08c1ac3d27
madddmike <mdshaw89@gmail.com>
2020-07-04 14:51:48 -0400

    Modified for Xbian
    Changed user "kodi" to "xbian".
    Changed systemctl stop line to "service xbmc stop".  Xbian uses "Upstart" not systemd.

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -2,7 +2,7 @@
 
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
-sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
-sudo systemctl stop kodi
+sudo openvt -c 7 -s -f -- su xbian -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
+sudo service xbmc stop
 
 exit
